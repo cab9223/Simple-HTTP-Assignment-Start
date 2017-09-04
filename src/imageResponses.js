ab5b22745f7e828b6d004fc6357ad7beeccc1e26
Christopher Bennett <cab9223@rit.edu>
2017-09-04 19:02:15 -0400

    Finished it all

---
+++ src/imageResponses.js
@@ -0,0 +1,15 @@
+
+const fs = require('fs'); // pull in the file system module
+
+const spongegar = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+
+const getSpongegar = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(spongegar);
+  response.end();
+};
+
+
+module.exports.getSpongegar = getSpongegar;
+
